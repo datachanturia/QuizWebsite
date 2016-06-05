a2300f65a3f4a984594389f8390e93cfc1ff227d
abuac13 <abuac13@freeuni.edu.ge>
2016-06-06 00:49:51 +0400

    Implement Answerdao

--- src/Database/AnswerDaoImpl.java
+++ src/Database/AnswerDaoImpl.java
@@ -1,31 +1,66 @@
 package Database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import Model.Answer;
+import Model.Question;
 
 public class AnswerDaoImpl implements AnswerDao {
 
+	private Connection con;
+	
 	public AnswerDaoImpl(Connection con) {
-		// TODO Auto-generated constructor stub
+		this.con = con;
 	}
 
 	@Override
 	public ArrayList<Answer> getQuestionAnswers(int questionID) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Answer> answers = new ArrayList<Answer>();
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Answers where questionID = " + questionID
+					+ "and isdelete = 0");
+			while(rs.next()){
+				Answer ans = new Answer(rs.getString("answer"), rs.getBoolean("iscorrect"));
+				answers.add(ans);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return answers;
 	}
 
 	@Override
 	public void addAnswer(int questionID, Answer answer) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO Answers (answer, iscorrect, "
+					+ "questionID) VALUES(?,?,?)");
+			preparedStatement.setString(1, answer.getAnswer());
+			preparedStatement.setBoolean(2, answer.getCorrect());
+			preparedStatement.setInt(3, questionID);
+			preparedStatement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public void deleteAnswer(int answerID) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement preparedStatement = con.prepareStatement("update Answers set isdelete = ? "
+					+ "where answerID = " + answerID);
+			preparedStatement.setInt(1, 1);
+			preparedStatement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
