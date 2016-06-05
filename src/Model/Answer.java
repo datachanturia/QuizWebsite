1516722f11a3dcbe3c083ced33e9165e07524586
abuac13 <abuac13@freeuni.edu.ge>
2016-06-05 17:39:42 +0400

    Add getCorrect() and setCorrect() methods

--- src/Model/Answer.java
+++ src/Model/Answer.java
@@ -4,16 +4,24 @@ public class Answer {
 	private String asnwer;
 	private boolean isCorrect;
 	
-	public Answer(String answer,boolean isCorrect) {
+	public Answer(String answer, boolean isCorrect) {
 		this.asnwer = answer;
 		this.isCorrect = isCorrect;
 	}
 
 	public String getAsnwer() {
-		return asnwer;
+		return this.asnwer;
 	}
 
 	public void setAsnwer(String asnwer) {
 		this.asnwer = asnwer;
+	}
+	
+	public boolean getCorrect(){
+		return this.isCorrect;
+	}
+	
+	public void setCorrect(boolean isCorrect){
+		this.isCorrect = isCorrect;
 	}
 }
