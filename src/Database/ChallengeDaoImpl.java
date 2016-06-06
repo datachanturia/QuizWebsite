d194290d6aa76051949d40035378fc32f3bdc694
asakevarashvili <asake13@freeuni.edu.ge>
2016-06-06 23:20:57 +0300

    Implemented ChallengeDao

--- src/Database/ChallengeDaoImpl.java
+++ src/Database/ChallengeDaoImpl.java
@@ -1,32 +1,71 @@
 package Database;
 
+
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import Model.Challenge;
+import Model.Request;
 
 public class ChallengeDaoImpl implements ChallengeDao {
 
+	private Connection con;
+	
 	public ChallengeDaoImpl(Connection con) {
-		// TODO Auto-generated constructor stub
+		this.con = con;
 	}
 
 	@Override
 	public ArrayList<Challenge> getUserChallenges(int userID) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Challenge> challenges = new ArrayList<Challenge>();
+		
+		try {
+			Statement st = con.createStatement();
+			ResultSet result = st.executeQuery("select * from Challenges where (senderID = " + userID + " or receiverID = " + userID + ")"
+					+ " and isdelete = 0");
+			while(result.next()){
+				Challenge challenge = new Challenge(result.getInt("challengeID"), result.getInt("senderID"), 
+						result.getInt("recieverID"), result.getInt("quizID"), result.getDate("senddate"));
+				challenges.add(challenge);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return challenges;
 	}
 
 	@Override
 	public void addChallenge(Challenge challenge) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement prepst = con.prepareStatement("insert into Challenges (senderID,recieverID,quizID,sendDate) values (?,?,?,?)");
+			prepst.setInt(1, challenge.getSenderID());
+			prepst.setInt(2, challenge.getRecieverID());
+			prepst.setInt(3, challenge.getQuizID());
+			prepst.setDate(4, challenge.getSendDate());
+			prepst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
-	public void deleteChallenge(int ChallengeID) {
-		// TODO Auto-generated method stub
-
+	public void deleteChallenge(int challengeID) {
+		try {
+			PreparedStatement prepst = con.prepareStatement("update Challenges set isdelete = ? "
+					+ "where challengeID = " + challengeID);
+			prepst.setInt(1, 1);
+			prepst.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
