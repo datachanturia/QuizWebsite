05251a0ae111d368071f89fc6435e8b7719c5437
daTTaa <dchan13@freeuni.edu.ge>
2016-06-09 23:44:12 +0400

    few additions

---
+++ src/ULS/AccountManagerTest.java
@@ -0,0 +1,18 @@
+package ULS;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+
+import org.junit.Test;
+
+public class AccountManagerTest {
+
+	@Test
+	public void test() throws ClassNotFoundException, SQLException, CloneNotSupportedException, InterruptedException {
+		//AccountManager am = new AccountManager();
+		//assertTrue(am.matchesAccount("dchan11@freeuni.edu.ge", "a"));
+		
+	}
+}
