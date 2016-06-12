9b7c3a1821535f779aac4505eb54cb4dd8176482
daTTaa <dchan13@freeuni.edu.ge>
2016-06-13 03:16:14 +0400

    everything with pooling

---
+++ src/dataSrc/MyDBInfo.java
@@ -0,0 +1,9 @@
+package dataSrc;
+
+public class MyDBInfo {
+	
+	 public static final String MYSQL_USERNAME = "root";
+	 public static final String MYSQL_PASSWORD = "";
+	 public static final String MYSQL_DATABASE_SERVER = "localhost:3306";
+	 public static final String MYSQL_DATABASE_NAME = "c_cs108_psyoung";
+}
