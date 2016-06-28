3c0a73c2843a2bea220011ca21d7a298476e7ed0
daTTaa <dchan13@freeuni.edu.ge>
2016-06-29 00:15:37 +0400

    index page done2

--- src/dataSrc/DataSource.java
+++ src/dataSrc/DataSource.java
@@ -8,7 +8,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 
 public class DataSource {
 
-	private static DataSource datasource;
+	private  DataSource datasource;
 	private BasicDataSource ds;
 
 	private DataSource() {
