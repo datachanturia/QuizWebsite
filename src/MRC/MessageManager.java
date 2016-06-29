fcb03369fd4887250459e8361287eafc4027376a
abuac13 <abuac13@freeuni.edu.ge>
2016-06-30 02:25:32 +0400

    Manage recieving and sending messages

--- src/MRC/MessageManager.java
+++ src/MRC/MessageManager.java
@@ -1,13 +1,35 @@
 package MRC;
 
+import Model.Message;
 import Model.User;
-import ULS.AccountManager;
 import java.sql.Connection;
+import java.util.ArrayList;
+
+import Database.MessageDaoImpl;
 
 public class MessageManager {
 	
+	private MessageDaoImpl mdi;
+	private int userID;
+	
 	public MessageManager(Connection con){
 		
+	}
+	
+	public void setConnection(java.sql.Connection conn){
+		this.mdi = new MessageDaoImpl(conn);
+	}
+	
+	public ArrayList<Message> getMessages(){
+		return mdi.getUserMessages(userID);
+	}
+	
+	public void sendMessage(Message mess){
+		mdi.addMessage(mess);
+	}
+	
+	public void setUserID(int userId){
+		this.userID = userId;
 	}
 	
 	
