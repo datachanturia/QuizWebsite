903b1c7c4608578d1ec8bdcde3ab8273e49462fc
abuac13 <abuac13@freeuni.edu.ge>
2016-06-30 02:26:55 +0400

    Take all messages

--- src/MRC/ServletMessage.java
+++ src/MRC/ServletMessage.java
@@ -1,6 +1,10 @@
 package MRC;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ULS.AccountManager;
+import dataSrc.DataSource;
 
 /**
  * Servlet implementation class ServletMessage
@@ -31,7 +36,30 @@ public class ServletMessage extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
+		Connection con = null;
+		
+		RequestDispatcher rd;
+		
 		MessageManager mm = (MessageManager) getServletContext().getAttribute("MessMan");
+		int userID = Integer.parseInt(request.getParameter("usId"));
+		mm.setUserID(userID);
+		
+		try {
+			con = DataSource.getInstance().getConnection();
+			mm.setConnection(con);
+
+			rd = request.getRequestDispatcher("./MRC/MessageList.jsp");
+			request.setAttribute("MessManager", mm);
+			
+			rd.forward(request, response);
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
 	}
 
 	/**
