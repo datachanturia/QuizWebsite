f92a6c1cf318657dbbdda1284cb1fefb4ecf58d3
daTTaa <dchan13@freeuni.edu.ge>
2016-06-30 21:53:11 +0400

    friends search

---
+++ src/MiddleArea/friendsSearchServlet.java
@@ -0,0 +1,78 @@
+package MiddleArea;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.QuizDaoImpl;
+import Model.Quiz;
+import ULS.AccountManager;
+import dataSrc.DataSource;
+
+/**
+ * Servlet implementation class friendsSearchServlet
+ */
+@WebServlet("/friendsSearchServlet")
+public class friendsSearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public friendsSearchServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		Connection con = null;
+
+		RequestDispatcher rd;
+		AccountManager am = (AccountManager) getServletContext().getAttribute("AccMan");
+
+		try {
+			con = DataSource.getInstance().getConnection();
+
+
+			request.setAttribute("accManager", am);
+
+			rd = request.getRequestDispatcher("./searchFriends/user/searchUserFriends.jsp");
+
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
