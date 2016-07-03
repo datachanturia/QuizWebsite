8b8adbae11c633d4248072a759926d94502d32cb
abuac13 <abuac13@freeuni.edu.ge>
2016-07-03 04:36:58 +0400

    Prepare message and mark as read in database

---
+++ src/MRC/ServletReadMesage.java
@@ -0,0 +1,90 @@
+package MRC;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.MessageDaoImpl;
+import Database.QuizDaoImpl;
+import Model.Quiz;
+import ULS.AccountManager;
+import dataSrc.DataSource;
+
+/**
+ * Servlet implementation class ServletReadMesage
+ */
+@WebServlet("/ServletReadMesage")
+public class ServletReadMesage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletReadMesage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		Connection con = null;
+
+		RequestDispatcher rd;
+
+		AccountManager am = (AccountManager) getServletContext().getAttribute("AccMan");
+
+		try {
+			con = DataSource.getInstance().getConnection();
+			MessageDaoImpl mdi = new MessageDaoImpl(con);
+			int messageID = Integer.parseInt(request.getParameter("messageID"));
+			mdi.read(messageID);
+
+			// ------------------------------------------
+			QuizDaoImpl qdi = new QuizDaoImpl(con);
+
+			request.setAttribute("accManager", am);
+
+			ArrayList<Quiz> dayPopuLs = qdi.getDayPopularQuiz();
+			ArrayList<Quiz> popQuizLs = qdi.getPopularQuiz();
+			ArrayList<Quiz> newQuizLs = qdi.getNewQuiz();
+
+			request.setAttribute("dayPopuLs", dayPopuLs);
+			request.setAttribute("popQuizLs", popQuizLs);
+			request.setAttribute("newQuizLs", newQuizLs);
+			// --------------------------------------------------
+
+			rd = request.getRequestDispatcher("./MRC/Message.jsp");
+
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
