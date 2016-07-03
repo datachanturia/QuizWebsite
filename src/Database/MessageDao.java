fb4998e22ccdfa989b422719b67ebe77f33dc355
abuac13 <abuac13@freeuni.edu.ge>
2016-07-03 04:34:04 +0400

    Add read() function

--- src/Database/MessageDao.java
+++ src/Database/MessageDao.java
@@ -9,4 +9,5 @@ public interface MessageDao {
 	public ArrayList<Message> getUserMessages(int userID);
 	public void addMessage(Message message);
 	public void deleteMessage(int messageID);
+	public void read(int messageID);
 }
