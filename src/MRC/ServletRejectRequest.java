6ea823b50a9a4a374e405a7f866c83b3933342af
abuac13 <abuac13@freeuni.edu.ge>
2016-07-03 20:35:20 +0400

    Reject friend request, remove request

---
+++ src/MRC/ServletRejectRequest.java
@@ -0,0 +1,97 @@
+package MRC;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.QuizDaoImpl;
+import Database.RequestDaoImpl;
+import Model.Quiz;
+import ULS.AccountManager;
+import dataSrc.DataSource;
+
+/**
+ * Servlet implementation class ServletRejectRequest
+ */
+@WebServlet("/ServletRejectRequest")
+public class ServletRejectRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletRejectRequest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		Connection con = null;
+
+		RequestDispatcher rd;
+
+		AccountManager am = (AccountManager) getServletContext().getAttribute("AccMan");
+
+		int requestID = Integer.parseInt(request.getParameter("requestID"));
+		int userID = am.getUser().getUserID();
+		try {
+			con = DataSource.getInstance().getConnection();
+			RequestDaoImpl rdi = new RequestDaoImpl(con);
+			rdi.deleteRequest(requestID);
+			
+			RequestManager rm = new RequestManager(con);
+			rm.setUserID(userID);
+			rm.setRequests(rdi.getUserRequests(userID));
+			
+			request.setAttribute("RequManager", rm);
+
+			// ------------------------------------------
+			QuizDaoImpl qdi = new QuizDaoImpl(con);
+
+			request.setAttribute("accManager", am);
+
+			ArrayList<Quiz> dayPopuLs = qdi.getDayPopularQuiz();
+			ArrayList<Quiz> popQuizLs = qdi.getPopularQuiz();
+			ArrayList<Quiz> newQuizLs = qdi.getNewQuiz();
+
+			request.setAttribute("dayPopuLs", dayPopuLs);
+			request.setAttribute("popQuizLs", popQuizLs);
+			request.setAttribute("newQuizLs", newQuizLs);
+			// --------------------------------------------------
+
+			rd = request.getRequestDispatcher("./MRC/RequestList.jsp");
+
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
