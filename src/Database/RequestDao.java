b73cd2563dd061b9e88dd00daf61c139ab4b4c37
abuac13 <abuac13@freeuni.edu.ge>
2016-07-03 20:30:59 +0400

    Add addFriend method

--- src/Database/RequestDao.java
+++ src/Database/RequestDao.java
@@ -8,5 +8,6 @@ public interface RequestDao {
 	
 	public ArrayList<Request> getUserRequests(int userID);
 	public void addRequest(Request request);
+	public void addFriend(int userID, int friendID);
 	public void deleteRequest(int requestID);
 }
