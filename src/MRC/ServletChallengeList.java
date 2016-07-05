a7715914050dc2c193ab325527fde69d07e49033
abuac13 <abuac13@freeuni.edu.ge>
2016-07-05 23:52:23 +0400

    Prepare user's challenges for listing

---
+++ src/MRC/ServletChallengeList.java
@@ -0,0 +1,96 @@
+package MRC;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.ChallengeDaoImpl;
+import Database.QuizDaoImpl;
+import Database.RequestDaoImpl;
+import Model.Quiz;
+import ULS.AccountManager;
+import dataSrc.DataSource;
+
+/**
+ * Servlet implementation class ServletChallengeList
+ */
+@WebServlet("/ServletChallengeList")
+public class ServletChallengeList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletChallengeList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		Connection con = null;
+
+		RequestDispatcher rd;
+
+		AccountManager am = (AccountManager) getServletContext().getAttribute("AccMan");
+
+		int userID = am.getUser().getUserID();
+		try {
+			con = DataSource.getInstance().getConnection();
+			
+			ChallengeDaoImpl cdi = new ChallengeDaoImpl(con);
+			ChallengeManager cm = new ChallengeManager(con);
+			cm.setUserID(userID);
+			cm.setChallenges(cdi.getUserChallenges(userID));
+			
+			request.setAttribute("ChallManager", cm);
+
+			// ------------------------------------------
+			QuizDaoImpl qdi = new QuizDaoImpl(con);
+
+			request.setAttribute("accManager", am);
+
+			ArrayList<Quiz> dayPopuLs = qdi.getDayPopularQuiz();
+			ArrayList<Quiz> popQuizLs = qdi.getPopularQuiz();
+			ArrayList<Quiz> newQuizLs = qdi.getNewQuiz();
+
+			request.setAttribute("dayPopuLs", dayPopuLs);
+			request.setAttribute("popQuizLs", popQuizLs);
+			request.setAttribute("newQuizLs", newQuizLs);
+			// --------------------------------------------------
+
+			rd = request.getRequestDispatcher("./MRC/ChallengeList.jsp");
+
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
