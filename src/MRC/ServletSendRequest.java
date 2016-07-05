001e74c98624808982e268b9bd739ca3b526f785
abuac13 <abuac13@freeuni.edu.ge>
2016-07-05 18:09:28 +0400

    Create Request and send

--- src/MRC/ServletSendRequest.java
+++ src/MRC/ServletSendRequest.java
@@ -1,11 +1,24 @@
 package MRC;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Database.QuizDaoImpl;
+import Database.RequestDaoImpl;
+import Model.Quiz;
+import Model.Request;
+import ULS.AccountManager;
+import dataSrc.DataSource;
 
 /**
  * Servlet implementation class ServletSendRequest
@@ -28,6 +41,54 @@ public class ServletSendRequest extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		Connection con = null;
+
+		RequestDispatcher rd;
+
+		AccountManager am = (AccountManager) getServletContext().getAttribute("AccMan");
+
+		int sender = am.getUser().getUserID();
+		int receiver = Integer.parseInt(request.getParameter("receiver"));
+		Calendar calendar = Calendar.getInstance();
+		java.util.Date currentDate = calendar.getTime();
+		java.sql.Date date = new java.sql.Date(currentDate.getTime());
+		Request r = new Request(0, sender, receiver, date, "");
+
+		try {
+
+			con = DataSource.getInstance().getConnection();
+
+			RequestDaoImpl rdi = new RequestDaoImpl(con);
+
+			rdi.addRequest(r);
+
+			// ------------------------------------------
+			QuizDaoImpl qdi = new QuizDaoImpl(con);
+
+			request.setAttribute("accManager", am);
+
+			ArrayList<Quiz> dayPopuLs = qdi.getDayPopularQuiz();
+			ArrayList<Quiz> popQuizLs = qdi.getPopularQuiz();
+			ArrayList<Quiz> newQuizLs = qdi.getNewQuiz();
+
+			request.setAttribute("dayPopuLs", dayPopuLs);
+			request.setAttribute("popQuizLs", popQuizLs);
+			request.setAttribute("newQuizLs", newQuizLs);
+			// --------------------------------------------------
+			
+			rd = request.getRequestDispatcher("./MRC/SentRequest.jsp");
+			
+
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		rd.forward(request, response);
 	}
 
 	/**
