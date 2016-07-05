dd74f041bfc062938632e9e0717d311c8a14cae5
daTTaa <dchan13@freeuni.edu.ge>
2016-07-06 03:05:02 +0400

    major bugs fixed

--- src/Database/PostDaoImpl.java
+++ src/Database/PostDaoImpl.java
@@ -24,7 +24,7 @@ public class PostDaoImpl implements PostDao {
 
 		try {
 			Statement st = con.createStatement();
-			ResultSet result = st.executeQuery("select * from AdminPosts order by messageID desc limit 4");
+			ResultSet result = st.executeQuery("select * from AdminPosts order by messageID desc limit 10");
 
 			while (result.next()) {
 				Post post = new Post(result.getInt("messageID"), result.getInt("adminID"),
